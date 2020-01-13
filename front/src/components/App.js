c1ce7a3407bafed8df3ac7ea1925114d0b800444
uzresk <uzr.esk@gmail.com>
2020-01-13 15:14:09 +0900

    use setState

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,62 +1,83 @@
 import React, {useState, useEffect, useContext, useReducer} from "react";
 import "../App.css";
+import reducer from '../reducers'
 import Header from "./Header";
 import Movie from "./Movie";
 import Search from "./Search";
+import AppContext from "../contexts/AppContext";
+import {SEARCH_MOVIES_REQUEST} from "../actions";
 
 const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey="; // you should replace this with yours
-
 const App = () => {
 
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const [errorMessage, setErrorMessage] = useState(null);
+        const initialState = {
+            loading: true,
+            movies: [],
+            errorMessage: null
+        };
+        const [state, setState] = useState(initialState);
+        const {loading, movies, errorMessage} = state;
 
-    useEffect(() => {
-        fetch(MOVIE_API_URL)
-            .then(response => response.json())
-            .then(jsonResponse => {
-                setMovies(jsonResponse.Search);
-                setLoading(false);
+        useEffect(() => {
+            console.log("init")
+            // dispatch({
+            //     type: SEARCH_MOVIES_REQUEST
+            // });
+            fetch(MOVIE_API_URL)
+                .then(response => response.json())
+                .then(jsonResponse => {
+                    setState({
+                        ...state,
+                        movies: jsonResponse.Search,
+                        loading: false
+                    })
+                })
+        }, []);
+
+        const search = searchValue => {
+            setState({
+                ...state,
+                loading: true,
+                errorMessage: null,
             });
-    }, []);
 
-    const search = searchValue => {
-        setLoading(true);
-        setErrorMessage(null);
+            fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=`)
+                .then(response => response.json())
+                .then(jsonResponse => {
+                    if (jsonResponse.Response === "True") {
+                        setState({
+                            ...state,
+                            movies: jsonResponse.Search,
+                            loading: false,
+                        });
+                    } else {
+                        setState({
+                            ...state,
+                            loading: false,
+                            errorMessage: jsonResponse.Error
+                        });
+                    }
+                });
+        };
 
-        fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=`)
-            .then(response => response.json())
-            .then(jsonResponse => {
-                if (jsonResponse.Response === "True") {
-                    setMovies(jsonResponse.Search);
-                    setLoading(false);
-                } else {
-                    setErrorMessage(jsonResponse.Error);
-                    setLoading(false);
-                }
-            });
-    };
 
-
-    return (
-        <div className="App">
-            <Header text="HOOKED" />
-            <Search search={search} />
-            <p className="App-intro">Sharing a few of our favourite movies</p>
-            <div className="movies">
-                {loading && !errorMessage ? (
-                    <span>loading...</span>
-                ) : errorMessage ? (
-                    <div className="errorMessage">{errorMessage}</div>
-                ) : (
-                    movies.map((movie, index) => (
-                        <Movie key={`${index}-${movie.Title}`} movie={movie} />
-                    ))
-                )}
+        return (
+            <div className="App">
+                <Header text="HOOKED"/>
+                <Search search={search}/>
+                <p className="App-intro">Sharing a few of our favourite movies</p>
+                <div className="movies">
+                    {loading && !errorMessage ? (
+                        <span>loading...</span>
+                    ) : errorMessage ? (
+                        <div className="errorMessage">{errorMessage}</div>
+                    ) : (
+                        movies.map((movie, index) => (<Movie key={index} movie={movie}/>))
+                    )}
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+;
 
 export default App;
